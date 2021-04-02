c9ac36cf2d50e8b74a3b2374fd674aae66564dfc
pijetaomirko <valentinoromansi@gmail.com>
2021-04-02 02:51:33 +0200

    /backtest -> reading data and backtesting updated

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { json } from "express"
 import { StockData } from "./stock/stock-data"
 import { Direction } from "./types/direction"
 import { BacktestResult } from "./backtest/backtest-result"
@@ -6,6 +6,7 @@ import { isPatternValid } from "./pattern-validator/pattern-validator"
 import { Strategy } from "./strategy/strategy"
 import { StrategyBacktestResults } from "./backtest/strategy-backtest-results"
 import { ApiReceiver } from "./data-extractor/api-receiver"
+import { JsonManager } from "./data-extractor/json-manager"
 
 function setHeaders(res: any) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
@@ -31,30 +32,37 @@ app.get("/update-stock-data", async (req: any, res: any) => {
 
 // Do backtest
 app.get("/backtest", async (req, res) => {
-  /*
   console.log("/backtest called...")
   setHeaders(res)
-
   const strategy: Strategy = Strategy.copy(req.body)
-  let stocksData: StockData[] = await apiReceiver.updateStockData()
-
-  let strategyBacktestResults = new StrategyBacktestResults(strategy.name, [])
-
-  stocksData.forEach((stock) => {
-    let backtestData = new BacktestResult(stock.symbol, 1)
-    stock.first().executeEachIteration(Direction.RIGHT, stock.length() - 1, (slice) => {
-      // Test if pattern is valid for given slice
-      if (isPatternValid(slice, strategy.rules)) {
-        backtestData.doBacktest(slice, strategy)
-      }
-      return true
-    })
-    strategyBacktestResults.backtestResults.push(backtestData)
-  })
-
   console.log(strategy.description())
+  let strategyBacktestResults = new StrategyBacktestResults(strategy.name, [])
+  const fs = require("fs")
+  const stocksPath = `src/resources/json/stocks`
 
+  const intervals: string[] = fs.readdirSync(stocksPath).map((file: string) => file)
+  for (const interval of intervals) {
+    let stocks: StockData[] = []
+    const fileNames: string[] = fs.readdirSync(`${stocksPath}/${interval}`).map((file: string) => file)
+    for (const fileName of fileNames) {
+      const jsonData = fs.readFileSync(`${stocksPath}/${interval}/${fileName}`, {
+        encoding: "utf8",
+        flag: "r",
+      })
+      stocks = StockData.getParsedJsonData(jsonData)
+
+      // For stock read from current json file
+      for (const stock of stocks) {
+        let backtestData = new BacktestResult(stock.symbol, 6)
+        stock.first().executeEachIteration(Direction.RIGHT, stock.length() - 1, (slice) => {
+          // Execute backtest if pattern is valid for given slice
+          if (isPatternValid(slice, strategy.rules)) backtestData.doBacktest(slice, strategy)
+          return true
+        })
+        strategyBacktestResults.backtestResults.push(backtestData)
+      }
+    }
+  }
   res.send(JSON.stringify(strategyBacktestResults))
   console.log("/backtest ended...")
-*/
 })
--- src/stock/stock-data.ts
+++ src/stock/stock-data.ts
@@ -39,4 +39,29 @@ export class StockData {
     })
     return stockData
   }
+
+  static getParsedJsonData(json: string): StockData[] {
+    let stocksData: StockData[] = []
+    const objList = JSON.parse(json)
+    for (const obj of objList) {
+      let stock: StockData = new StockData()
+      stock.symbol = obj.symbol
+      stock.interval = obj.interval
+      stock.fundamentals = obj.fundamentals
+      for (const jsonSlice of obj.slices) {
+        stock.append(
+          new VerticalSlice(
+            jsonSlice.date,
+            jsonSlice.open,
+            jsonSlice.close,
+            jsonSlice.high,
+            jsonSlice.low,
+            jsonSlice.volume
+          )
+        )
+      }
+      stocksData.push(stock)
+    }
+    return stocksData
+  }
 }
